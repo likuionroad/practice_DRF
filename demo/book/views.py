e7f5825ac40407f1adb0137715d918be5859283b
likuionroad <1021335035@qq.com>
2019-04-18 16:22:05 +0800

    实验数据库

--- demo/book/views.py
+++ demo/book/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+from .models import BookInfo,HeroInfo
+
 # Create your views here.   safe=False
 
 
 def first(request):
     a = [{"name": 123, "age": 23}, {"name": 234, "age": 25}]
     a = json.dumps(a)
-
+    # a = BookInfokInfo.objects.all()
+    # list1 = []
+    # for i in a:
+    #     print(i,i.bread)
+    #     list1.append(i.bread)
+    #     print(list1)
+    # list1 = json.dumps(list1)
     return HttpResponse(a)
